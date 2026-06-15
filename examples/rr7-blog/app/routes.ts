1cfa6114f421350b4ab892e847dcb672efc5c6bc
vanya2h <hi@vanya2h.me>
2026-06-15 23:52:04 +0100

    feat(example): add posts list route with SSR'd rxfy data

--- examples/rr7-blog/app/routes.ts
+++ examples/rr7-blog/app/routes.ts
@@ -1,3 +1,3 @@
-import { index, type RouteConfig } from "@react-router/dev/routes";
+import { index, route, type RouteConfig } from "@react-router/dev/routes";
 
-export default [index("routes/_index.tsx")] satisfies RouteConfig;
+export default [index("routes/_index.tsx"), route("posts", "routes/posts.tsx")] satisfies RouteConfig;
---
+++ examples/rr7-blog/app/routes/posts.tsx
@@ -0,0 +1,5 @@
+import PostList from "../components/PostList";
+
+export default function PostsRoute() {
+  return <PostList />;
+}
