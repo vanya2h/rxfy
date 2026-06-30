ca46c37581b3abbb4c34c18b0c2beab232755582
vanya2h <hi@vanya2h.me>
2026-06-30 22:55:18 +0100

    chore(rxfy-ws): scaffold package

---
+++ packages/rxfy-ws/tsup.config.ts
@@ -0,0 +1,13 @@
+import path from "node:path";
+import { defineConfig } from "tsup";
+import { config } from "./config.js";
+
+export default defineConfig({
+  format: ["cjs", "esm"],
+  dts: true,
+  outDir: config.distDir,
+  entry: {
+    index: path.join(config.srcDir, "index.ts"),
+    client: path.join(config.srcDir, "client.ts"),
+  },
+});
