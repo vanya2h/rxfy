93f8aab6b5352693172cc3662c03ee3bbf3dd8b0
Koriakovtcev Ivan <beatzhitta@gmail.com>
2026-07-03 18:01:16 +0200

    chore(examples): remove vite-realtime-todos, superseded by vite-blog-framework

--- examples/vite-realtime-todos/shared/todo.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const TodoSchema = z.object({
-  id: z.string(),
-  title: z.string(),
-  done: z.boolean(),
-});
-
-export type Todo = z.infer<typeof TodoSchema>;
