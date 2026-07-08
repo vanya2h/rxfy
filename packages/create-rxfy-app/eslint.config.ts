35808db959d76988135e675ef1ccc10589b5dd0b
vanya2h <hi@vanya2h.me>
2026-07-08 16:36:42 +0100

    feat(create-rxfy-app): package shell and scaffold logic

---
+++ packages/create-rxfy-app/eslint.config.ts
@@ -0,0 +1,7 @@
+import { config } from "@vanya2h/eslint-config/base";
+import { Linter } from "eslint";
+
+export default [
+  ...config,
+  { ignores: ["dist/**", ".turbo/**", "node_modules/**", "*.tsbuildinfo"] },
+] satisfies Linter.Config[];
